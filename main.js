1c2faf09404ebc4ee33dff0cea5104cfdf40c7e5
Oscar Rivera <argo.weibo@protonmail.com>
2023-05-15 19:48:41 -0500

    Clase 20, agregamos el menú en mobile, y se ajusta para el responsive design

--- main.js
+++ main.js
@@ -1,10 +1,20 @@
 const menuEmail = document.querySelector('.navbar-email');
+const menuBurger = document.querySelector('.menu');
+
 const desktopMenu = document.querySelector('.desktop-menu');
+const mobileMenu = document.querySelector('.mobile-menu');
 
 menuEmail.addEventListener('click', toogleDesktopMenu);
+menuBurger.addEventListener('click', toogleMobileMenu);
 
 function toogleDesktopMenu(event)
 {
     console.log('Click');
     desktopMenu.classList.toggle('inactive');
+}
+
+function toogleMobileMenu(event)
+{
+    console.log('Click');
+    mobileMenu.classList.toggle('inactive');
 }
