4d232b7f79395cb3f94f35d56e6bc1034721cca2
Oscar Rivera <argo.weibo@protonmail.com>
2023-05-15 20:26:11 -0500

    Clase 21, agregamos el menú de ordenes, y ajustamos la intersección entre menús para que no se superpongan

--- main.js
+++ main.js
@@ -1,20 +1,43 @@
 const menuEmail = document.querySelector('.navbar-email');
 const menuBurger = document.querySelector('.menu');
+const menuShopCart = document.querySelector('.navbar-shopping-cart');
 
 const desktopMenu = document.querySelector('.desktop-menu');
 const mobileMenu = document.querySelector('.mobile-menu');
+const asideMyOrder = document.querySelector('.product-detail');
 
 menuEmail.addEventListener('click', toogleDesktopMenu);
 menuBurger.addEventListener('click', toogleMobileMenu);
+menuShopCart.addEventListener('click', toogleShopCart);
 
 function toogleDesktopMenu(event)
 {
-    console.log('Click');
+    const isShopCartOpen = !asideMyOrder.classList.contains('inactive');
+    if(isShopCartOpen)
+    {
+        asideMyOrder.classList.add('inactive');
+    }
     desktopMenu.classList.toggle('inactive');
 }
 
 function toogleMobileMenu(event)
 {
-    console.log('Click');
+    const isShopCartOpen = !asideMyOrder.classList.contains('inactive');
+    if(isShopCartOpen)
+    {
+        asideMyOrder.classList.add('inactive');
+    }
     mobileMenu.classList.toggle('inactive');
+}
+
+function toogleShopCart(event)
+{
+    const isMobileMenuOpen = !mobileMenu.classList.contains('inactive');
+    const isDesktopMenuOpen = !desktopMenu.classList.contains('inactive');
+    if(isMobileMenuOpen || isDesktopMenuOpen)
+    {
+        mobileMenu.classList.add('inactive');
+        desktopMenu.classList.add('inactive');
+    }
+    asideMyOrder.classList.toggle('inactive');
 }
